e6d639560e258f1d4d5733e32d15c644cca52639
Bhaskar Goyal <bhaskargoyal24@gmail.com>
2017-06-20 11:58:45 +0530

    testController

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
 const express= require('express');
+const bodyParser = require('body-parser');
+const http = require('http');
+const path = require('path');
+const chalk = require('chalk');
+const session = require('express-session');
+
+
+const testController = require('./controllers/test');
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: process.env.SESSION_SECRET,
+  store: new MongoStore({
+    url: process.env.MONGODB_URI || process.env.MONGOLAB_URI,
+    autoReconnect: true,
+    clear_interval: 3600
+  })
+}));
+app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
+
+
+app.get('/test', testController.test);
+
+
+
+
+
+app.listen(app.get('port'), () => {
+  console.log('%s App is running at http://localhost:%d in %s mode', chalk.green('✓'), app.get('port'), app.get('env')); 
+  console.log('  Press CTRL-C to stop\n');
+});
+
+module.exports = app;
